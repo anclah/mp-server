a31f1b74b21203505dab3d01fd75c652a3b913b4
anclah <ansila1230@gmail.com>
2024-03-20 21:22:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//importing json-server package
+const JSONserver=require("json-server")
+//creating server using craete()method from json-server
+const MPserver=JSONserver.create()
+//generating path/route to server resource
+const router =JSONserver.router('db.json')
+//initiating json-server middlewares
+const middleware=JSONserver.defaults()
+//creating port number for server
+const PORT=3000 | process.env.PORT
+//implementing middleware to server
+MPserver.use(middleware)
+//implementing route to resource to server
+MPserver.use(router)
+//run command for server
+MPserver.listen(PORT,()=>{
+    console.log (`Server is running at:${PORT}`)
+})
